e6ee3b76c32854f7032d467058a6a1de6787c228
bitotassuya <jareansong@gmail.com>
2022-05-12 09:16:36 +0700

    Updates

---
+++ precache-manifest.7b76ad0678b37e1d43d6a47c47aad497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b95b7b92a7f3e5c8f52",
+    "url": "/basic_progress_app/static/css/main.c9f180a9.chunk.css"
+  },
+  {
+    "revision": "2b95b7b92a7f3e5c8f52",
+    "url": "/basic_progress_app/static/js/main.2b95b7b9.chunk.js"
+  },
+  {
+    "revision": "778716355edb4a0e9910",
+    "url": "/basic_progress_app/static/js/1.77871635.chunk.js"
+  },
+  {
+    "revision": "4bcb75ae572a7b43cfc8",
+    "url": "/basic_progress_app/static/js/runtime~main.4bcb75ae.js"
+  },
+  {
+    "revision": "475e8fb27891419941b66ded7311e46d",
+    "url": "/basic_progress_app/index.html"
+  }
+];
